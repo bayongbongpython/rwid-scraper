a8d9c2694bb670d28e854fd13de9a375aab49b43
andi <andi.hidayat.mail@gmail.com>
2020-12-25 08:20:26 +0700

    res.html

--- run.py
+++ run.py
@@ -1,8 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
+from requests import Session
+
+session = requests.Session()
 
 def login():
     print('login...')
+    datas = {
+        'username': 'user',
+        'password': 'user12345'
+    }
+
+    res = Session.post('http://127.0.0.1:5000/login', data=datas)
+
+    f = open('./res.html', 'w+')
+    f.write(res.text)
+    f.close()
+
+    soup = BeautifulSoup(res.text, 'html5lib')
+
+    page_item = soup.find_all('li', attrs={'class': 'page-item'})
+    print_pages = len(page_item) - 2
+    return total_pages
 
 def get_urls():
     print('getting urls...')
